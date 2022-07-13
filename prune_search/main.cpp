d0ca9fc8608356f4fb5431508b1b85d3e3d4e966
void-hoge <coilgunrailgun@gmail.com>
2022-07-13 17:52:37 +0900

    init

---
+++ prune_search/main.cpp
@@ -0,0 +1,76 @@
+// 27019578
+// Mugi Noda
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+std::mt19937 mt(2);
+
+template<typename T>
+void print_vec(std::vector<T> vec) {
+	// print the single dimention std::vector<T> VEC.
+	if (vec.size() != 0) {
+		std::cout << "["<< vec.at(0);
+		for (std::size_t i = 1; i < vec.size(); i++) {
+			std::cout << ", " << vec.at(i);
+		}
+		std::cout << "]" << std::endl;
+	}
+}
+
+std::size_t partition(std::vector<int>& vec, const std::size_t left, const std::size_t right) {
+	// Gather the elements blow pivot on the left side, and returns the index of the pivot after the operation.
+	// pivot is the leftmost element.
+	auto pivot = vec.at(left);
+	std::swap(vec.at(left), vec.at(right-1));
+	auto tmp = left;
+	for (auto i = left; i < right; i++) {
+		if (vec.at(i) < pivot) {
+			std::swap(vec.at(tmp), vec.at(i));
+			tmp++;
+		}
+	}
+	std::swap(vec.at(right-1), vec.at(tmp));
+	return tmp;
+}
+
+int min_kth(std::vector<int> vec, const std::size_t k) {
+	// Returns the kth element in ascending order.
+	std::size_t left = 0, right = vec.size();
+	while (left != right) {
+		auto tmp = partition(vec, left, right);
+		if (tmp > k) {
+			right--;
+		} else if (tmp < k) {
+			left = tmp+1;
+		}else {
+			break;
+		}
+	}
+	return vec.at(k);
+}
+
+int min_kth_sort(std::vector<int> vec, const std::size_t k) {
+	std::sort(vec.begin(), vec.end());
+	return vec.at(k);
+}
+
+int main() {
+	std::vector<int> vec;
+	for (int i = 0; i < 10000; i++) {
+		vec.push_back(i);
+	}
+	std::shuffle(vec.begin(), vec.end(), mt);
+	//print_vec(vec);
+	for (std::size_t i = 0; i < 10000; i++) {
+		auto tmp = min_kth(vec, i);
+		std::cout << i << " " << min_kth(vec, i) << std::endl;
+//		std::cout << i << " " << min_kth_sort(vec, i) << std::endl;
+		if (i != tmp) {
+			break;
+		}
+	}
+
+	return 0;
+}
---
+++ triangle_partition/main.cpp
@@ -0,0 +1,25 @@
+#include "minkth.hpp"
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+std::mt19937 mt(0);
+
+int main() {
+	std::vector<int> vec;
+	for (int i = 0; i < 10000; i++) {
+		vec.push_back(i);
+	}
+	std::shuffle(vec.begin(), vec.end(), mt);
+//	std::cout << vec << std::endl;
+	for (std::size_t i = 0; i < 10000; i++) {
+//		auto tmp = voidhoge::min_kth(vec, i);
+		auto tmp = voidhoge::min_kth_sort(vec, i);
+		std::cout << i << " " << tmp << std::endl;
+		if (i != tmp) {
+			break;
+		}
+	}
+	return 0;
+}
